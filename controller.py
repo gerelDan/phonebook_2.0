09cd47638087a397fd8abff19f750e3542243af7
gerel <gerel@bk.ru>
2024-03-04 20:43:30 +0500

    add OOP in model.py

--- controller.py
+++ controller.py
@@ -5,7 +5,7 @@ from copy import deepcopy
 
 def find_contacts(message):
     search_word = view.input_data(message)
-    result = model.find_contact(search_word)
+    result = model.phone_book.find_contact(search_word)
     view.show_contacts(result, text.find_contact_no_result(search_word))
     return True if result else False
 
@@ -14,19 +14,19 @@ def start_app():
         user_choice = view.show_main_menu()
         match user_choice:
             case 1:
-                model.open_phone_book()
+                model.phone_book.open_phone_book()
                 view.show_message(text.phone_book_opened_successful)
 
             case 2:
-                model.save_phone_book()
+                model.phone_book.save_phone_book()
                 view.show_message(text.phone_book_saved_successful)
 
             case 3:
-                view.show_contacts(model.phone_book, text.empty_phone_book_error)
+                view.show_contacts(model.phone_book.phone_book, text.empty_phone_book_error)
 
             case 4:
                 new_contact = view.input_data(text.input_new_contact)
-                model.add_new_contact(new_contact)
+                model.phone_book.add_new_contact(new_contact)
                 view.show_message(text.new_contact_added_successful(new_contact[0]))
 
             case 5:
@@ -36,22 +36,22 @@ def start_app():
                 if find_contacts(text.input_search_word_for_edit):
                     u_id = int(view.input_data(text.input_id_for_edit))
                     edited_contact = view.input_data(text.edit_contact)
-                    name = model.edit_contact(u_id, edited_contact)
+                    name = model.phone_book.edit_contact(u_id, edited_contact)
                     view.show_message(text.edit_contact_successful(name))
 
             case 7:
                 if find_contacts(text.input_search_word_for_delete):
                     u_id = int(view.input_data(text.input_id_for_delete))
-                    name = model.delete_contact(u_id)
+                    name = model.phone_book.delete_contact(u_id)
                     view.show_message(text.delete_contact_successful(name))
 
             case 8:
-                phonebook_tmp = deepcopy(model.phone_book)
-                model.open_phone_book()
-                if phonebook_tmp != model.phone_book:
+                phonebook_tmp = deepcopy(model.phone_book.phone_book)
+                model.phone_book.open_phone_book()
+                if phonebook_tmp != model.phone_book.phone_book:
                     user_choice = view.show_exit_menu()
                     if user_choice == 1:
-                        model.phone_book = phonebook_tmp
-                        model.save_phone_book()
+                        model.phone_book.phone_book = phonebook_tmp
+                        model.phone_book.save_phone_book()
                         view.show_message(text.phone_book_saved_successful)
                 break
--- model.py
+++ model.py
@@ -1,51 +1,50 @@
-phone_book = {}
-path = 'phonebook.txt'
-SEPARATOR = ';'
+# phone_book = {}
+# path = 'phonebook.txt'
+# SEPARATOR = ';'
+class Phone_book:
+    def __init__(self, path: str, separator: str):
+        self.phone_book = {}
+        self.path = path
+        self.separator = separator
 
-def open_phone_book():
-    global phone_book
-    with open(path, 'r', encoding='UTF-8') as file:
-        data = file.readlines()
-    for u_id, contact in enumerate(data, 1):
-        phone_book[u_id] = contact.strip().split(SEPARATOR)
+    def open_phone_book(self):
+        with open(self.path, 'r', encoding='UTF-8') as file:
+            data = file.readlines()
+        for u_id, contact in enumerate(data, 1):
+            self.phone_book[u_id] = contact.strip().split(self.separator)
 
-def save_phone_book():
-    global phone_book
-    data = []
-    for contact in phone_book.values():
-        data.append(SEPARATOR.join(contact))
-    data = '\n'.join(data)
-    with open(path, 'w', encoding='UTF-8') as file:
-        file.write(data)
+    def save_phone_book(self):
+        data = []
+        for contact in self.phone_book.values():
+            data.append(self.separator.join(contact))
+        data = '\n'.join(data)
+        with open(self.path, 'w', encoding='UTF-8') as file:
+            file.write(data)
 
-def _next_uid():
-    global phone_book
-    return max(phone_book) + 1 if phone_book else 1
+    def _next_uid(self):
+        return max(self.phone_book) + 1 if self.phone_book else 1
 
-def add_new_contact(new_contact: list[str]):
-    global phone_book
-    phone_book[_next_uid] = new_contact
+    def add_new_contact(self, new_contact: list[str]):
+        self.phone_book[self._next_uid] = new_contact
 
-def find_contact(search_word: str) -> dict[int, list[str]]:
-    global phone_book
-    result = {}
-    for u_id, contact in phone_book.items():
-        if search_word.lower() in ' '.join(contact).lower():
-            result[u_id] = contact
-    return result
+    def find_contact(self, search_word: str) -> dict[int, list[str]]:
+        result = {}
+        for u_id, contact in self.phone_book.items():
+            if search_word.lower() in ' '.join(contact).lower():
+                result[u_id] = contact
+        return result
 
-def edit_contact(u_id: int, edited_contact: list[str]) -> str:
-    '''
-    Функция изменит контакт в телефонной книге
-    '''
-    global phone_book
-    current_contact = phone_book[u_id]
-    for i in range(len(current_contact)):
-        current_contact[i] = edited_contact[i] if edited_contact[i] else current_contact[i]
-    phone_book[u_id] = current_contact
-    return current_contact[0]
+    def edit_contact(self, u_id: int, edited_contact: list[str]) -> str:
+        '''
+        Метод изменит контакт в телефонной книге
+        '''
+        current_contact = self.phone_book[u_id]
+        for i in range(len(current_contact)):
+            current_contact[i] = edited_contact[i] if edited_contact[i] else current_contact[i]
+        self.phone_book[u_id] = current_contact
+        return current_contact[0]
 
-def delete_contact(u_id: int) -> str:
-    global phone_book
-    return phone_book.pop(u_id)[0]
+    def delete_contact(self, u_id: int) -> str:
+        return self.phone_book.pop(u_id)[0]
 
+phone_book = Phone_book('phonebook.txt', ';')
